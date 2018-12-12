3da2fba2c808093bb1b819390978c5ed1a82c0e4
PankajB1997 <pankajbhootra@gmail.com>
2018-12-12 23:06:58 +0530

    Add export to excel function

--- src/public/controllers/HomeController.js
+++ src/public/controllers/HomeController.js
@@ -5,41 +5,71 @@
 
     HomeController.$inject = ["$location", "toaster", "RepositoryService"];
 
+    function preprocessor(objArray) {
+        //TODO: Need to preprocess data to make it suitable for appearance in Excel
+        return objArray;
+    }
+
+    function ConvertToCSV(objArray) {
+        objArray = preprocessor(objArray);
+        var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+        var str = '';
+        var row = "";
+        for (var index in objArray[0]) {
+            row += index + ',';
+        }
+        row = row.slice(0, -1);
+        str += row + '\r\n';
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in array[i]) {
+                if (line != '')
+                    line += ',';
+                line += array[i][index];
+            }
+            str += line + '\r\n';
+        }
+        return str;
+    }
+
     function HomeController($location, toaster, repository) {
         var vm = this;
-
         vm.bills = [];
-
         vm.search = {};
-
-        // toaster.pop("wait", "Loading bills...");
-
         repository.getBills(vm.search).then(function (result) {
             vm.bills = result.data;
         });
-
         vm.add = function () {
             $location.path("/bill/add/");
         };
-
         vm.addMaster = function () {
             $location.path("/bill/add-master/");
         }
-
         vm.search = function () {
             repository.getBills(vm.search).then(function (result) {
                 vm.bills = result.data;
             });
         };
-
+        vm.exportToExcel = function () {
+            repository.getBills(vm.search).then(function (result) {
+                var csvData = ConvertToCSV(result.data);
+                var a = document.createElement("a");
+                a.setAttribute('style', 'display:none;');
+                document.body.appendChild(a);
+                // TODO: Make sure data appears in a presentable manner inside the csv
+                var blob = new Blob([csvData], { type: 'text/csv' });
+                var url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = 'results.csv';
+                a.click();
+            });
+        }
         vm.details = function (id) {
             $location.path("/bill/details/" + id);
         };
-
         vm.remove = function (id) {
             $location.path("/bill/remove/" + id);
         };
-
         vm.downloadClaim = function (id) {
             repository.getBill(id).then(function (result) {
                 const doc = new jsPDF();
