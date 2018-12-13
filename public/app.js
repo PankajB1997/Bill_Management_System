5b9271d400d169d47bd9ba72cc3e8e5616d9a7d8
PankajB1997 <pankajbhootra@gmail.com>
2018-12-13 23:32:42 +0530

    Add payment status on HTML side

--- public/app.js
+++ public/app.js
@@ -20,12 +20,12 @@ var module = angular.module("billManager", [
                 controllerAs: "vm"
             })
             .when("/bill/add-master", {
-                templateUrl: base + "master/master.html",
+                templateUrl: base + "master/index.html",
                 controller: "MasterController",
                 controllerAs: "vm"
             })
             .when("/bill/add-master/:id", {
-                templateUrl: base + "master/master_edit.html",
+                templateUrl: base + "master/edit.html",
                 controller: "MasterEditController",
                 controllerAs: "vm"
             })
